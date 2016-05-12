2432b8e4902670d946f7e94ee5fde5ef5c527a42
Henrique Silveira <hd2312@columbia.edu>
2016-05-12 10:42:30 -0300

    Added RSA class for RSA encryption

---
+++ JohnsSmartHome/src/johnssmarthome/RSAGHM.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package johnssmarthome;
+
+import java.math.BigInteger;
+
+/**
+ *
+ * @author henrisilver
+ */
+public class RSAGHM {
+    private BigInteger e, d, n;
+    
+    public RSAGHM() {
+        
+    }
+    
+    public String encrypt(String message) {
+        return null;
+    }
+    
+    public String decrypt(String ciphertext) {
+        return null;
+    }
+}
