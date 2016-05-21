333d189e70b7b78d60c0964c30c52b6895e191a0
giulianoprado <giulianoprado@gmail.com>
2016-05-21 00:02:06 -0300

    cesar added

--- JohnsSmartHome/src/johnssmarthome/RSAGHM.java
+++ JohnsSmartHome/src/johnssmarthome/RSAGHM.java
@@ -15,12 +15,14 @@ import java.security.SecureRandom;
  */
 public class RSAGHM {
     private BigInteger e, d, n;
+    private int cesarCode;
     
     private static final int NUMBITS = 1024;
     private static final int PRIME_PROBABILITY = 100;
     
     public RSAGHM() {
         SecureRandom randomGenerator = new SecureRandom();
+        cesarCode = randomGenerator.nextInt()%256;
         BigInteger p = new BigInteger(NUMBITS, PRIME_PROBABILITY,randomGenerator);
         BigInteger q = new BigInteger(NUMBITS, PRIME_PROBABILITY,randomGenerator);
         
@@ -44,6 +46,26 @@ public class RSAGHM {
     public void setPublicKey(BigInteger d) {
         this.d = d;
     }
+
+    public BigInteger getPrivateKey() {
+        return e;
+    }
+
+    
+    public BigInteger getPublicKey() {
+        return d;
+    }
+
+
+    public int getCesarCode() {
+        return cesarCode;
+    }
+
+    public void setCesarCode(int cesarCode) {
+        this.cesarCode = cesarCode;
+    }
+    
+    
     
     private void getCoprime(BigInteger n, BigInteger z) {
         e = n.subtract(BigInteger.ONE);
@@ -51,12 +73,15 @@ public class RSAGHM {
         // While we do not get an e such that it is coprime with z, and while
         // it is greater than one, keep searching for e.
         while(e.gcd(z).compareTo(BigInteger.ONE) != 0 && e.compareTo(BigInteger.ONE) == 1) {
-            e.subtract(BigInteger.ONE);
+            e = e.subtract(BigInteger.ONE);
         }
     }
     
     public BigInteger encrypt(String message) {
         byte messageBytes[] = message.getBytes();
+        for (int i=0;i<messageBytes.length;i++) {
+            messageBytes[i] = (byte)((messageBytes[i]+cesarCode)%256);
+        }
         BigInteger messageEncrypted = new BigInteger(messageBytes).modPow(e, n);
         return messageEncrypted;
         //return (new String(messageEncrypted.toByteArray(), StandardCharsets.UTF_8));
@@ -67,7 +92,11 @@ public class RSAGHM {
         //byte messageBytes[] = ciphertext.getBytes();
         //BigInteger messagePlain = new BigInteger(messageBytes).modPow(d, n);
         //return (new String(messagePlain.toByteArray(), StandardCharsets.UTF_8));
-        return new String(ciphertext.modPow(d, n).toByteArray(), StandardCharsets.UTF_8);
+        byte[] messageBytes = ciphertext.modPow(d, n).toByteArray();
+        for (int i=0;i<messageBytes.length;i++) {
+            messageBytes[i] = (byte)((messageBytes[i]+256-cesarCode)%256);
+        }
+        return new String(messageBytes, StandardCharsets.UTF_8);
     }
     
 }
